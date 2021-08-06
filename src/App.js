43beac425ba5b6e4de9b492afdbcbcfd46d33358
yoshi59 <87689874+yoshi59@users.noreply.github.com>
2021-08-07 08:17:14 +0900

    main

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello <code>src/App.js</code> and save to reload.
+          こちらは、VSコードから記入しました。 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
