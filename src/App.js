c93b60a1fadb45cb05f01c763329c4e7496664d8
yoshi59 <87689874+yoshi59@users.noreply.github.com>
2021-08-07 08:59:21 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
 
-// こちらはVSCODEです。こんにちは！
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          こちらは、VSコードから記入しました。 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>pizzazzza</h1>
+      <Navbar />
     </div>
   );
 }
